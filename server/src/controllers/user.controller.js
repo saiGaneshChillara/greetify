0d8086fb33894ab65491eaed683847cc65e306e9
cvlnpsg987 <cvlnpsg987@gmail.com>
2025-07-17 11:34:40 +0000

    sendFriendRequest controller added

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import FriendRequest from "../models/friendRequest.model.js";
 import User from "../models/user.model.js";
 
 export const getRecommendedUsers = async (req, res) => {
@@ -37,6 +38,48 @@ export const getMyFriends = async (req, res) => {
 };
 
 export const sendFriendRequest = async (req, res) => {
+    try {
+        const myId = req.user._id;
+        const { id: recipientId } = req.params;
 
+        // can't send request to your self
+        if (myId === recipientId) return res.status(400).json({
+            message: "You can't send friend request to yourself",
+        });
+
+        // check if recipient exists
+        const recipient =  await User.findById(recipientId);
+        if (!recipient) return res.status(404).json({
+            message: "Recipient not found",
+        });
+
+        // check if already both are friends
+        if (recipient.friends.includes(myId)) return res.status(400).json({
+            message: "You are already friends with this user",
+        });
+
+        // check if there is already a pending request
+        const existingRequest = await FriendRequest.findOne({
+            $or: [
+                { sender: myId, recipient: recipientId },
+                { sender: recipientId, recipient: myId },
+            ]
+        });
+        if (existingRequest) return res.status(400).json({
+            message: "Friend request already exists",
+        });
+
+        // create a new friend request
+        const newRequest = await FriendRequest.create({
+            sender: myId,
+            recipient: recipientId,
+        });
+
+        return res.status(201).json(newRequest);
+    } catch (err) {
+        console.log("Error in sendFriendRequest controller:", err);
+        return res.status(500).json({
+            message: "Internal server error",
+        });
+    }
 };
-
