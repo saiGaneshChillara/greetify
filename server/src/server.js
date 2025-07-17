69003efedf402eb107a7399e7b2e03997f2694e5
cvlnpsg987 <cvlnpsg987@gmail.com>
2025-07-17 18:12:09 +0000

    tanstack, axios, cors added

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
@@ -12,6 +13,12 @@ dotenv.config();
 
 const app = express();
 
+app.use(
+    cors({
+        origin: "https://5173-firebase-greetify-1751898334967.cluster-44kx2eiocbhe2tyk3zoyo3ryuo.cloudworkstations.dev",
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(cookieParser());
 
